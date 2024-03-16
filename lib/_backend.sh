3b2bdb1ea97629ce9a3ead9cc535b000e1dddf36
unknown <danielct59@gmail.com>
2024-03-16 15:39:50 -0300

    first commit

---
+++ lib/_backend.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+# functions for setting up app backend
+
+
+#######################################
+# sets environment variable for backend.
+# Arguments:
+#   None
+#######################################
+backend_extract_ura() {
+  print_banner
+  printf "${WHITE} 💻 Extraindo os arquivos da URA..."
+  printf "\n\n"
+  sleep 2
+  
+  sleep 2
+
+}
+
+#######################################
+# sets environment variable for backend.
+# Arguments:
+#   None
+#######################################
+backend_copy_ura() {
+  print_banner
+  printf "${WHITE} 💻 Descompactando os arquivos da URA..."
+  printf "\n\n"
+
+  sleep 2
+  
+  cp -r /root/ISSABEL-PBX/agi-bin /var/lib/asterisk/agi-bin
+  cp -r /root/ISSABEL-PBX/sounds /var/lib/asterisk/sounds/en
+  cp -r /root/ISSABEL-PBX/conf /etc/asterisk/
+
+
+
+
+  sleep 2
+}
+
+#######################################
+# sets environment variable for backend.
+# Arguments:
+#   None
+#######################################
+backend_copy_send_bol_pdf() {
+  print_banner
+  printf "${WHITE} 💻 Reiniciando Serviço (Asterisk)..."
+  printf "\n\n"
+
+  sleep 2
+  service asterisk restart
+  sleep 2
+}
