5e63d49208b2d9ca9b87201d939c2a9e1258b2b6
skynetfibragithub <161240475+skynetfibragithub@users.noreply.github.com>
2024-04-02 01:44:58 -0300

    Update _backend.sh

--- lib/_backend.sh
+++ lib/_backend.sh
@@ -32,8 +32,8 @@ backend_copy_ura() {
   
   cp -r /root/ISSABEL-PBX/agi-bin/cron_fat_mensal.php /var/lib/asterisk/agi-bin
   cp -r /root/ISSABEL-PBX/agi-bin/ura_consulta.php /var/lib/asterisk/agi-bin
-  cp -r /root/ISSABEL-PBX/sounds/* /var/lib/asterisk/en/
-  cp -r /root/ISSABEL-PBX/conf /etc/asterisk/
+  cp -r /root/ISSABEL-PBX/sounds/* /var/lib/asterisk/en
+  cp -r /root/ISSABEL-PBX/conf/* /etc/asterisk/
 
 
 
